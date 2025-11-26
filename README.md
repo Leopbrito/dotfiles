51b21a249209ddec3ecfe9b844273c24a2a8b63d
Leonardo Brito <leopbrito@icloud.com>
2025-11-26 20:31:53 -0300

    add `install.sh`

---
+++ README.md
@@ -0,0 +1,6 @@
+# dotfiles
+
+to install:
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Leopbrito/dotfiles/HEAD/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,68 @@
+magenta() {
+	MAGENTA="\033[35m"
+	RESET="\033[0m"
+	echo -e "${MAGENTA}$1${RESET}"
+}
+
+magenta  "Starting terminal configuration..."
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sudo apt install git -y
+fi
+
+magenta "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+magenta "Installing git"
+brew install git
+
+magenta "Installing git-delta"
+brew install git-delta
+
+magenta "Installing yadm"
+brew install yadm
+
+magenta "Installing gh"
+brew install gh
+
+magenta "Installing zsh"
+brew install zsh
+
+magenta "Installing Starship"
+brew install starship
+
+magenta "Installing zsh-autosuggestions"
+brew install zsh-autosuggestions
+
+magenta "Installing zsh-syntax-highlighting"
+brew install zsh-syntax-highlighting
+
+magenta "Installing eza"
+brew install eza
+
+magenta "Installing nvm"
+brew install nvm
+
+magenta "Installing jenv"
+brew install jenv
+
+magenta "Installing yq"
+brew install yq
+
+magenta "Installing netcat"
+brew install netcat
+
+if [[ -n "$BASH_VERSION" ]]; then
+
+	magenta "Making Zsh the default shell"
+	chsh -s $(which zsh)
+
+	magenta "Getting dotfiles config"
+	yadm clone https://github.com/Leopbrito/dotfiles.git
+
+	zsh
+fi
